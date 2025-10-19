318cc0fba1df7986b76c6e03c8e45c67767d2262
pratyush9892 <pratyush85790@gmail.com>
2025-10-19 07:11:34 +0530

    footer change hero navbar

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -32,9 +32,9 @@ const Footer = () => {
         {
             title: "CONTACT",
             links: [
-                { text: "+1-212-456-7890", path: '/', icon: MailIcon },
-                { text: "contact@example.com", path: '/', icon: PhoneIcon },
-                { text: "794 Francisco, 94102", path: '/', icon: MapPinIcon }
+                { text: "+91 8579027841", path: '/', icon: MailIcon },
+                { text: "techpqr@gmail.com", path: '/', icon: PhoneIcon },
+                { text: "NIT Nagaland", path: '/', icon: MapPinIcon }
             ]
         }
     ];
@@ -43,7 +43,7 @@ const Footer = () => {
         { icon: FacebookIcon, link: "https://www.facebook.com" },
         { icon: InstagramIcon, link: "https://www.instagram.com" },
         { icon: TwitterIcon, link: "https://twitter.com" },
-        { icon: LinkedinIcon, link: "https://www.linkedin.com" },
+        { icon: LinkedinIcon, link: "https://www.linkedin.com/in/pratyush9892/" },
     ]
 
     return (
@@ -52,9 +52,9 @@ const Footer = () => {
                 <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-slate-500/30 text-slate-500">
                     <div>
                         <Link href="/" className="text-4xl font-semibold text-slate-700">
-                            <span className="text-green-600">go</span>cart<span className="text-green-600 text-5xl leading-0">.</span>
+                            <span className="text-green-600">mus</span>atra<span className="text-green-600 text-5xl leading-0">.</span>
                         </Link>
-                        <p className="max-w-[410px] mt-6 text-sm">Welcome to gocart, your ultimate destination for the latest and smartest gadgets. From smartphones and smartwatches to essential accessories, we bring you the best in innovation — all in one place.</p>
+                        <p className="max-w-[410px] mt-6 text-sm">Welcome to Musatra — a world where technology meets sophistication. Discover a curated selection of next-generation smartphones, smartwatches, and accessories designed to elevate everyday living. At Musatra, innovation isn’t just delivered — it’s experienced.</p>
                         <div className="flex items-center gap-3 mt-5">
                             {socialIcons.map((item, i) => (
                                 <Link href={item.link} key={i} className="flex items-center justify-center w-10 h-10 bg-slate-100 hover:scale-105 hover:border border-slate-300 transition rounded-full">
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -12,26 +12,26 @@ const Hero = () => {
     return (
         <div className='mx-6'>
             <div className='flex max-xl:flex-col gap-8 max-w-7xl mx-auto my-10'>
-                <div className='relative flex-1 flex flex-col bg-green-200 rounded-3xl xl:min-h-100 group'>
+                <div className='relative flex-1 flex flex-col bg-gradient-to-r from-[#0f0f0f] to-[#1a1a1a] rounded-3xl xl:min-h-100 group shadow-2xl'>
                     <div className='p-5 sm:p-16'>
-                        <div className='inline-flex items-center gap-3 bg-green-300 text-green-600 pr-4 p-1 rounded-full text-xs sm:text-sm'>
-                            <span className='bg-green-600 px-3 py-1 max-sm:ml-1 rounded-full text-white text-xs'>NEWS</span> Free Shipping on Orders Above $50! <ChevronRightIcon className='group-hover:ml-2 transition-all' size={16} />
+                        <div className='inline-flex items-center gap-3 bg-[#1e1e1e] text-[#00FFB2] pr-4 p-1 rounded-full text-xs sm:text-sm'>
+                            <span className='bg-[#00FFB2] px-3 py-1 max-sm:ml-1 rounded-full text-black text-xs font-semibold'>NEWS</span> Free Shipping on Orders Above $50! <ChevronRightIcon className='group-hover:ml-2 transition-all' size={16} />
                         </div>
-                        <h2 className='text-3xl sm:text-5xl leading-[1.2] my-3 font-medium bg-gradient-to-r from-slate-600 to-[#A0FF74] bg-clip-text text-transparent max-w-xs  sm:max-w-md'>
-                            Gadgets you'll love. Prices you'll trust.
+                        <h2 className='text-3xl sm:text-5xl leading-[1.2] my-3 font-semibold bg-gradient-to-r from-white to-[#00FFB2] bg-clip-text text-transparent max-w-xs sm:max-w-md drop-shadow-[0_2px_10px_rgba(0,0,0,0.4)]'>
+                            Musatra--Innovation you love. Value you trust.
                         </h2>
-                        <div className='text-slate-800 text-sm font-medium mt-4 sm:mt-8'>
+                        <div className='text-gray-300 text-sm font-medium mt-4 sm:mt-8'>
                             <p>Starts from</p>
-                            <p className='text-3xl'>{currency}4.90</p>
+                            <p className='text-3xl font-bold text-[#00FFB2]'>{currency}9.9</p>
                         </div>
-                        <button className='bg-slate-800 text-white text-sm py-2.5 px-7 sm:py-5 sm:px-12 mt-4 sm:mt-10 rounded-md hover:bg-slate-900 hover:scale-103 active:scale-95 transition'>LEARN MORE</button>
+                        <button className='bg-[#00FFB2] text-black text-sm font-semibold py-2.5 px-7 sm:py-5 sm:px-12 mt-4 sm:mt-10 rounded-md hover:bg-[#00d999] hover:scale-105 active:scale-95 transition shadow-lg'>LEARN MORE</button>
                     </div>
-                    <Image className='sm:absolute bottom-0 right-0 md:right-10 w-full sm:max-w-sm' src={assets.hero_model_img} alt="" />
+                    <Image className='sm:absolute bottom-0 right-0 md:right-10 w-full sm:max-w-sm drop-shadow-[0_5px_25px_rgba(0,255,178,0.3)]' src={assets.hero_model_img} alt="" />
                 </div>
                 <div className='flex flex-col md:flex-row xl:flex-col gap-5 w-full xl:max-w-sm text-sm text-slate-600'>
                     <div className='flex-1 flex items-center justify-between w-full bg-orange-200 rounded-3xl p-6 px-8 group'>
                         <div>
-                            <p className='text-3xl font-medium bg-gradient-to-r from-slate-800 to-[#FFAD51] bg-clip-text text-transparent max-w-40'>Best products</p>
+                            <p className='text-3xl font-medium bg-gradient-to-r from-slate-800 to-[#FFAD51] bg-clip-text text-transparent max-w-40'>Trending Now</p>
                             <p className='flex items-center gap-1 mt-4'>View more <ArrowRightIcon className='group-hover:ml-2 transition-all' size={18} /> </p>
                         </div>
                         <Image className='w-35' src={assets.hero_product_img1} alt="" />
